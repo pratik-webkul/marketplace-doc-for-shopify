413738bdc53abd36c624f4d42a44f2cd1b0300af
pratik <pratik@webkul.com>
2025-07-03 17:25:00 +0530

    add node module in ignore

--- docs/.vuepress/.temp/git/config.js
+++
@@ -1,9 +0,0 @@
-import { GitContributors } from "/home/pratik/documentation/marketplace-doc/node_modules/@vuepress/plugin-git/lib/client/components/GitContributors.js";
-import { GitChangelog } from "/home/pratik/documentation/marketplace-doc/node_modules/@vuepress/plugin-git/lib/client/components/GitChangelog.js";
-
-export default {
-  enhance: ({ app }) => {
-    app.component("GitContributors", GitContributors);
-    app.component("GitChangelog", GitChangelog);
-  },
-};
--- docs/.vuepress/.temp/pages/configration/general-configration.html.vue
+++
@@ -1,8 +0,0 @@
-<template><div><h2 id="general-configuration" tabindex="-1"><a class="header-anchor" href="#general-configuration"><span><strong>General Configuration-</strong></span></a></h2>
-<p>This includes information that relate to the Admin account.</p>
-<p>From here, the admin can enter the business email, set the currency for the shop, choose the weight unit for products, and much more.</p>
-<p><img src="https://cdnblog.webkul.com/blog/wp-content/uploads/2017/11/Configuration-_-Admin.jpg" alt="General configuration Multivendor Marketplace for Shopify by Webkul"></p>
-<p>To know about the <strong>Configuration Updates</strong>, Kindly refer: <a href="https://webkul.com/blog/configuration-updates-multi-vendor-marketplace-for-shopify/" target="_blank" rel="noopener noreferrer">https://webkul.com/blog/configuration-updates-multi-vendor-marketplace-for-shopify/</a></p>
-</div></template>
-
-
